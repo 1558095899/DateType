b5e5f078ae9c83bfe97263b85a58a69a273cb824
1558095899 <1558095899@qq.com>
2019-01-21 15:57:24 +0800

    math

---
+++ Math/src/com/lzw/Math.java
@@ -0,0 +1,14 @@
+package com.lzw;
+
+public class Math {
+
+	public static void main(String[] args) {
+		Integer a=44;
+		System.out.println(a);// TODO 自动生成的方法存根
+		int[] arr= {1,2,3,4,5,6};
+		for(int i:arr) {
+			System.out.println(i);
+		}
+	}
+
+}
